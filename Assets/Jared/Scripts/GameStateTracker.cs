cbd6c84f89a4bf608bd00ff65a55d45121c86d1f
Jared Goronkin <jgoronkin@gmail.com>
2024-03-24 20:23:42 -0400

    Added timer, fail state, and win state for playtest

---
+++ Assets/Jared/Scripts/GameStateTracker.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateTracker : MonoBehaviour
+{
+    public enum GameState { Playing, Win, Fail }
+    [SerializeField]
+    private GameState _state = GameState.Playing;
+
+    [SerializeField]
+    private float GameLength = 30;
+    private float GameStart;
+
+    [SerializeField]
+    Transform BioMassParent;
+
+    private void Awake()
+    {
+        GameStart = Time.time;
+    }
+    void Update()
+    {
+        if(BioMassParent.childCount== 0)
+        {
+            _state = GameState.Win;
+        }
+        else if(GetGameTime()<=0)
+        {
+            _state = GameState.Fail;
+        }
+        switch(_state)
+        {
+            case GameState.Win:
+                SceneManager.LoadScene("WinScene");
+                break;
+            case GameState.Fail:
+                SceneManager.LoadScene("FailScene");
+                break;
+            case GameState.Playing:
+                break;
+        }
+    }
+    /// <summary>
+    /// Get the current time left in the game.
+    /// </summary>
+    /// <returns>Game time left in seconds.</returns>
+    public float GetGameTime()
+    {
+        return GameLength - (Time.time - GameStart);
+    }
+}
