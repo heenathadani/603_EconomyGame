087b2d5ab77875eff41b19862a718a17b6e87a39
Heena Thadani <heenathadani26@gmail.com>
2024-04-03 03:48:03 -0400

    added new icons and ui for abilities ui, icons for biomass and rna, still have to complete shop ui

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -111,8 +111,8 @@ public class HUDController : MonoBehaviour
                                 GetComponent<Animator>().Play("ErrorTextFade", -1, 0);
                             }
                         });
-                        //abilityButtons[i].GetComponent<Image>().sprite = a.abilitySprite;
                         TextMeshProUGUI[] texts = abilityButtons[i].GetComponentsInChildren<TextMeshProUGUI>();
+                        //abilityButtons[i].GetComponent<Image>().sprite = a.abilitySprite;
                         texts[0].text = a.abilityName;
                         texts[1].text = $"{a.cost} Biomass";
                         abilityButtons[i].gameObject.SetActive(true);
